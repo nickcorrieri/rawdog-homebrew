ea198065cc5059d4b14750741bb64266f47c16ea
Nick Corrieri <ncorrieri@gmail.com>
2026-05-21 01:50:02 -0500

    Update rawdog formula to 0.2.33

--- Formula/rawdog.rb
+++ Formula/rawdog.rb
@@ -5,8 +5,8 @@ class Rawdog < Formula
 
   desc "RAW photo managing tool that can fetch, copy, and audit your RAW libraries"
   homepage "https://github.com/nickcorrieri/rawdog"
-  url "https://github.com/nickcorrieri/rawdog/releases/download/v0.2.32/rawdog-0.2.32.tar.gz"
-  sha256 "96e4b0a005e6e82f121d0446f5fc95e5145e5ea4b147264f00060cf9fe704d62"
+  url "https://github.com/nickcorrieri/rawdog/releases/download/v0.2.33/rawdog-0.2.33.tar.gz"
+  sha256 "7d1177ef483495b061322066327991614a0a03cbe7f6fb6bafa0fa7a7cd7aa96"
   license "Apache-2.0"
 
   depends_on "python"
